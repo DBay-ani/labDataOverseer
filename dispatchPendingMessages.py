fc5525eb219341d76987e333af7d92c006f0b561
David (Daavood, داود) Bayani <dbayani@mit.edu>
2025-03-13 17:06:10 -0400

    Committing state of dispatchPendingMessages.py .

--- dispatchPendingMessages.py
+++ dispatchPendingMessages.py
@@ -18,3 +18,30 @@ from utils.handleError import handleError;
 
 
 
+def getMessagesCurrentlyPendingSend():
+    return 
+
+#TODO: dailly, email maintainers, let know how many messages queued have error state, and list the last 5 by date added...
+"""
+
+
+SELECT B.ID, B.message, A.name, A.contactInfo 
+FROM 
+    Contactors AS A, 
+    OutgoingMessageTable AS B  
+WHERE 
+    B.status = 'pending'  AND
+    ( 
+        ( (A.notifyAboutGeneralMaintenceAndInfo = 1) AND (B.isGeneralMaintenceAndInfo = 1) ) OR
+        ( (A.notifyWhenAdditions = 1) AND (B.isDataAddition = 1)) OR
+        ( (A.notifyWhenProblem = 1) AND (B.isProblem = 1)) 
+);
+
+SELECT ID, message, status FROM OutgoingMessageTable WHERE isGeneralMaintenceAndInfo = 0 AND isDataAddition = 0 AND isProblem = 0 ;
+    change status to error
+select the ones labeled "error", summarize them and send...    
+
+handle errors as also something written in the message queue....
+
+"""
+
