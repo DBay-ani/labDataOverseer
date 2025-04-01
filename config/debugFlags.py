694a9ebcc3edd9ee1abf292afed9c6dcb41e8551
David (Daavood, داود) Bayani <dbayani@mit.edu>
2025-04-01 14:20:30 -0400

    Stripped out referrence to Fanoos in the code itself.

--- config/debugFlags.py
+++ config/debugFlags.py
@@ -1,34 +1,3 @@
-# Fanoos: Multi-Resolution, Multi-Strength, Interactive Explanations for Learned Systems ; David Bayani and Stefan Mitsch ; paper at https://arxiv.org/abs/2006.12453
-# Copyright (C) 2021  David Bayani
-# 
-# This file is part of Fanoos.
-# 
-# Fanoos is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 3 of the License only.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# 
-# Contact Information:
-# 
-# Electronic Mail:
-#   dcbayani@alumni.cmu.edu
-# 
-# Paper Mail:
-#   David Bayani
-#   Computer Science Department
-#   Carnegie Mellon University
-#   5000 Forbes Ave.
-#   Pittsburgh, PA 15213
-#   USA
-# 
-# 
 
 
 v_print=0; # stands for verbrosity of printing.
